4d451a622755e1dfd2240edb4d1c4d0d8383d430
TungTop <stefanitvn@gmail.com>
2021-01-16 13:48:48 -0600

    value and reference problem

---
+++ Web/Javascript/value_reference.js
@@ -0,0 +1,25 @@
+// value types
+let a = 1;
+let b;
+
+b = a;
+a = 2;
+console.log(a, b);
+
+// reference types
+let a1 = {name: "Thanh Tung Truong"};
+let b1;
+
+b1 = a1;
+a1.name = "Thanh Tung Updated";
+
+console.log(a1, b1);
+
+
+
+
+
+
+
+
+
